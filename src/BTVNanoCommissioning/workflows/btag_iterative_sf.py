6ec61cae819b1a91fd2bb7ff9219dbd2515da51f
Felix Philipp Zinn <felix.zinn@rwth-aachen.de>
2025-08-15 09:08:09 +0200

    btag_iterative_sf: add logging

--- src/BTVNanoCommissioning/workflows/btag_iterative_sf.py
+++ src/BTVNanoCommissioning/workflows/btag_iterative_sf.py
@@ -1,3 +1,4 @@
+import logging
 import warnings
 from functools import reduce
 from operator import and_, or_
@@ -24,6 +25,10 @@ from BTVNanoCommissioning.utils.selection import (
     jet_id,
     mu_idiso,
 )
+
+# logger
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO)
 
 # ignore warnings from coffea
 warnings.filterwarnings(
@@ -100,6 +105,7 @@ def muon_selection(events, campaign: str):
     :return: selected muons
     :rtype: NanoAOD array
     """
+    logger.debug("Selecting muons")
     muons = events.Muon
     mask = (muons.pt > 15) & (mu_idiso(events, campaign))
     return muons[mask]
@@ -114,6 +120,7 @@ def electron_selection(events, campaign: str):
     :return: selected electrons
     :rtype: NanoAOD array
     """
+    logger.debug("Selecting electrons")
     electrons = events.Electron
     mask = (electrons.pt > 15) & (ele_mvatightid(events, campaign))
     return electrons[mask]
@@ -129,6 +136,7 @@ def jet_selection(events, campaign: str):
     :return: selected jets
     :rtype: NanoAOD array
     """
+    logger.debug("Selecting jets")
     jets = events.Jet
     mask = jet_id(events, campaign, max_eta=2.5, min_pt=20.0)
     return jets[mask]
@@ -191,6 +199,7 @@ HISTOGRAM_AXES = {
 
 
 def define_histograms(particle_objects: dict[str, list], b_taggers: Iterable[str], channels: Iterable[str]):
+    logger.debug("defining histograms")
     histograms = {
         "dr_jets": hist.Hist(
             HISTOGRAM_AXES["sys_axis"],
@@ -241,6 +250,7 @@ def fill_histograms(
     systematics: Iterable,
     isSyst: bool,
 ):
+    logger.debug("filling histograms")
     for syst in systematics:
         if not isSyst and syst != "nominal":
             break
@@ -286,8 +296,8 @@ def fill_histograms(
             syst=syst,
         )
 
+        logger.debug("filling b-tag discriminant histograms for syst %s", syst)
         for b_tagger in b_taggers:
-            # for now only 1 jet, no swapping
             for jet_index in (1, 0):
                 for region in ("HF", "LF"):
                     for channel in channels:
@@ -398,6 +408,7 @@ class BTagIterativeSFProcessor(processor.ProcessorABC):
 
     def process(self, events):
         events = missing_branch(events)
+        logger.info("preparing shifts")
         shifts = common_shifts(self, events)
 
         return processor.accumulate(
@@ -406,6 +417,7 @@ class BTagIterativeSFProcessor(processor.ProcessorABC):
         )
 
     def process_shift(self, events, shift_name):
+        logger.info("processing shift %s", shift_name)
         dataset = events.metadata["dataset"]
 
         # get histogram dict
@@ -423,6 +435,7 @@ class BTagIterativeSFProcessor(processor.ProcessorABC):
         ####################
 
         # lumi mask
+        logger.debug("Applying luminosity mask")
         lumi_mask = np.ones(len(events), dtype="bool")
         if isRealData:
             lumi_mask = self.lumiMask(events.run, events.luminosityBlock)
@@ -447,6 +460,7 @@ class BTagIterativeSFProcessor(processor.ProcessorABC):
             ],
         }
 
+        logger.debug("Getting trigger masks")
         trigger_masks = {
             channel: HLT_helper(events, trigger_paths)
             for channel, trigger_paths in triggers.items()
@@ -474,6 +488,7 @@ class BTagIterativeSFProcessor(processor.ProcessorABC):
         met_filter_mask = MET_filters(events, self._campaign)
 
         ### events selection ###
+        logger.debug("Event selection")
 
         # lepton cuts
         # combine leptons into a dilepton pair
@@ -605,6 +620,7 @@ class BTagIterativeSFProcessor(processor.ProcessorABC):
         # tag and probe
         # ==============
 
+        logger.debug("tag-and-probe")
         flavor_and_regions = {}
         for i_tag_jet, i_probe_jet in [(0, 1), (1, 0)]:
             # for swapping, use: [(0, 1), (1, 0)]
@@ -666,6 +682,7 @@ class BTagIterativeSFProcessor(processor.ProcessorABC):
         # ==============
         # weights
         # ==============
+        logger.debug("setting up weight_manager")
         weights = weight_manager(pruned_events, self.SF_map, self.isSyst)
         if shift_name is None:
             systematics = ["nominal"] + list(weights.variations)
